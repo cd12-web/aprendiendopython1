42398bd9c7eed8fd7b68d394ffac4b928f980d01
aleja <maria.escobarb@udea.edu.co>
2021-06-17 11:11:03 -0500

    Escribir la vista de detalles, resultados y votos configurar las rutas de las vistas Escirbir encuesta de votos Refactorizar las vistas para utilizar clases genéricas

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,34 +1,20 @@
-from django.db import models
-
-
-
-
-
-class Choice(models.Model):
-    question = models.ForeignKey('polls.Question', on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-from django.db import models
-
-
-class Choice(models.Model):
-    # ...
-    def __str__(self):
-        return self.choice_text
-
-
-from django.db import models
-
-# Create your models here.
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
 
+class Choice(models.Model):
+    question = models.ForeignKey('polls.Question', on_delete=models.CASCADE,blank=True,null=True)
+    choice_text = models.CharField(max_length=200,blank=True)
+    votes = models.IntegerField(default=0,blank=True)
+
+    def __str__(self):
+        return self.choice_text
+
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200,blank=True)
+    pub_date = models.DateTimeField('date published',blank=True)
 
     def __str__(self):
         return self.question_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,35 +1,26 @@
-
-
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 from django.urls import reverse
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-from django.http import HttpResponse
-
 from .models import Question, Choice
-from django.http import HttpResponse
-from django.template import loader
 
-from .models import Question
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -48,7 +39,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
 
 # Leave the rest of the views (detail, results, vote) unchanged
